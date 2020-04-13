0df4ca0b9966d3a215d5f896dc07b5f65a610cb7
mengxzh <mengxzh@weilaicheng.com>
2020-04-13 23:32:44 +0800

    init leetcode for jingying

---
+++ src/main/java/com/jingying/jingying/JingyingLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.jingying.jingying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingyingLeetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JingyingLeetcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jingying/jingying/JingyingLeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingying.jingying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingyingLeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
